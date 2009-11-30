f1daf4b52cf6d4cc9c9f179130b986f152c8c935
kassarar <kassarar@localhost>
2009-11-30 03:47:41 +0000

    Add a missed file

---
+++ mydt/types.h
@@ -0,0 +1,16 @@
+﻿// vim:fdm=marker:nu:nowrap:encoding=utf-8
+
+#ifndef _TYPES_H
+#define _TYPES_H
+
+#define uint8 unsigned char
+#define int8 char
+#define boolean unsigned char 
+#define byte unsigned char
+#define uint16 unsigned short
+#define int16 short
+#define uint32 unsigned int
+#define int32 int
+#define real float 
+
+#endif
