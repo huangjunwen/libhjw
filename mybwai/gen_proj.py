e7751846fb1e7f41f417a464d35571ffa25890bb
kassarar <kassarar@localhost>
2009-12-02 13:05:04 +0000

    Fix some bugs

--- mybwai/gen_proj.py
+++ mybwai/gen_proj.py
@@ -3,9 +3,15 @@ import os, re, uuid
 import os.path
 
 # do not put space in bwapi_dir
-bwapi_dir = "D:\\project\\myProject\\bwai\\BWAPI_Beta_2.4"
+conf_name = ".conf"
 tmpl_dir = "tmpl"
 libs = ["BWAPI.lib", "BWTA.lib", "CGAL-vc90-mt.lib", "gmp-vc90-mt.lib", "libboost_thread-vc90-mt-1_38.lib", "mpfr-vc90-mt.lib"]
+
+try:
+    bwapi_dir = open(conf_name).read().strip()
+except IOError:
+    bwapi_dir = raw_input("Please input your bwapi dir: ")
+    open(conf_name, 'w').write(bwapi_dir)
 
 while True:
     aimod_name = raw_input("Please input your new module's name: ")
@@ -24,6 +30,7 @@ for dirpath, dirnames, filenames in os.walk(tmpl_dir):
         s = open(os.path.join(tmpl_dir, filename)).read()
         s = s % d
         open(os.path.join(aimod_name, filename), 'w').write(s)
+    break
         
 
 
