cd76fc03ea54949c9d6a25a7a2f463c1871a589f
kassarar <kassarar@localhost>
2009-12-04 08:35:54 +0000

    Restart all

--- mybwai/AI1/aimod.cpp
+++ mybwai/AI1/aimod.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 using namespace BWAPI;
 
-void drawEdge(const node * n1, const node * n2) {
-    UnitData * d1 = (UnitData *)n1->attr;
-    UnitData * d2 = (UnitData *)n2->attr;
-    Unit * u1 = d1->unit;
-    Unit * u2 = d2->unit;
+void UnitPairHandler::operator() (Unit * u1, Unit * u2) {
     Color c;
     if (u1->getPlayer() == u2->getPlayer())
         c = Colors::Green;
@@ -20,36 +16,10 @@ void drawEdge(const node * n1, const node * n2) {
 }
 
 
-AI1::AI1():BWAPI::AIModule(), self(Broodwar->self()) {
-	dt_create(&dt);
-}
-
-AI1::~AI1() {
-	dt_destroy(&dt);
-}
-
-void AI1::add_unit(Unit * u) {
-    if (is_self(u))
-        self_units.add_unit(u);
-    else
-        enemy_units.add_unit(u);  
-}
-
-void AI1::remove_unit(Unit *u) {
-    if (is_self(u))
-        self_units.remove_unit(u);
-    else
-        enemy_units.remove_unit(u);
-}
-
 void AI1::onStart()
 {
     Broodwar->enableFlag(Flag::UserInput);
     //Broodwar->setLocalSpeed(20);
-    set<Unit *> & all_units = Broodwar->getAllUnits();
-    set<Unit *>::iterator iter;
-    for (iter = all_units.begin(); iter != all_units.end(); ++iter)
-        add_unit(*iter);
 }
 
 void AI1::onEnd(bool isWinner)
@@ -58,20 +28,19 @@ void AI1::onEnd(bool isWinner)
 
 void AI1::onFrame()
 {
-    UnitGrp::const_iterator iter;
+    set<Unit *> & all_units = Broodwar->getAllUnits();
+    set<Unit *>::iterator iter;
     Unit * u;
-    dt_begin(dt, drawEdge);
-    for (iter = self_units.begin(); iter != self_units.end(); ++iter) {
-        u = iter->first;
-        dt_next(dt, u->getPosition().x(), u->getPosition().y(), iter->second);
-    }
+    Broodwar->drawTextScreen(5, 10, "total units: %d\n", all_units.size());
 
-    for (iter = enemy_units.begin(); iter != enemy_units.end(); ++iter) {
-        u = iter->first;
-        dt_next(dt, u->getPosition().x(), u->getPosition().y(), iter->second);
+    UnitPairHandler handler;
+    dt.begin(handler);
+    for (iter = all_units.begin(); iter != all_units.end(); ++iter) {
+        u = (*iter);
+        Position pos = u->getPosition();
+        dt.next(pos.x(), pos.y(), u);
     }
-
-	dt_end(dt);
+    dt.end();
 
     set<Unit *> & selected = Broodwar->getSelectedUnits();
     set<Unit *>::iterator uiter = selected.begin();
@@ -88,12 +57,10 @@ void AI1::onFrame()
 
 void AI1::onUnitCreate(BWAPI::Unit* u)
 {
-    add_unit(u);
 }
 
 void AI1::onUnitDestroy(BWAPI::Unit* u)
 {
-    remove_unit(u);
 }
 
 void AI1::onUnitMorph(BWAPI::Unit* u)
@@ -102,12 +69,10 @@ void AI1::onUnitMorph(BWAPI::Unit* u)
 
 void AI1::onUnitShow(BWAPI::Unit* u)
 {
-    add_unit(u);
 }
 
 void AI1::onUnitHide(BWAPI::Unit* u)
 {
-    remove_unit(u);
 }
 
 void AI1::onUnitRenegade(BWAPI::Unit* unit)
--- mybwai/AI1/aimod.h
+++ mybwai/AI1/aimod.h
@@ -1,33 +1,28 @@
 #pragma once
 #include <BWAPI.h>
-#include <BWTA.h>
 #include <windows.h>
-#include "dt.h"
-#include "unit_data.h"
+#include "mydt.hpp"
+
+class UnitPairHandler {
+public:
+    void operator() (BWAPI::Unit *, BWAPI::Unit *);
+};
 
 class AI1: public BWAPI::AIModule
 {
 public:
-  virtual void onStart();
-  virtual void onEnd(bool isWinner);
-  virtual void onFrame();
-  virtual bool onSendText(std::string text);
-  virtual void onPlayerLeft(BWAPI::Player* player);
-  virtual void onNukeDetect(BWAPI::Position target);
-  virtual void onUnitCreate(BWAPI::Unit* unit);
-  virtual void onUnitDestroy(BWAPI::Unit* unit);
-  virtual void onUnitMorph(BWAPI::Unit* unit);
-  virtual void onUnitShow(BWAPI::Unit* unit);
-  virtual void onUnitHide(BWAPI::Unit* unit);
-  virtual void onUnitRenegade(BWAPI::Unit* unit);
-  AI1();
-  virtual ~AI1();
+    virtual void onStart();
+    virtual void onEnd(bool isWinner);
+    virtual void onFrame();
+    virtual bool onSendText(std::string text);
+    virtual void onPlayerLeft(BWAPI::Player* player);
+    virtual void onNukeDetect(BWAPI::Position target);
+    virtual void onUnitCreate(BWAPI::Unit* unit);
+    virtual void onUnitDestroy(BWAPI::Unit* unit);
+    virtual void onUnitMorph(BWAPI::Unit* unit);
+    virtual void onUnitShow(BWAPI::Unit* unit);
+    virtual void onUnitHide(BWAPI::Unit* unit);
+    virtual void onUnitRenegade(BWAPI::Unit* unit);
 private:
-    BWAPI::Player * const self;
-    bool is_self(BWAPI::Unit * u) { return u->getPlayer() == self; }
-    void add_unit(BWAPI::Unit * u);
-    void remove_unit(BWAPI::Unit *u);
-    UnitGrp self_units;
-    UnitGrp enemy_units;
-	myDt dt;
+    MyDT<BWAPI::Unit, UnitPairHandler> dt;
 };
