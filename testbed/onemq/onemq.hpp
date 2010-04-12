a506b16353c9c852fb1d24169b76cd028c548d60
kassarar <kassarar@localhost>
2010-04-12 13:49:24 +0000

    Add some notes

--- testbed/onemq/onemq.hpp
+++ testbed/onemq/onemq.hpp
@@ -9,14 +9,7 @@ typedef struct MessageID MessageID;
 typedef struct Message Message;
 
 
-struct Message {
-    const char * content;
-    size_t len;
-    MessageID id;
-};
-
-
-class _BaseObject {
+class _QActor {
 public:
     int last_err() { return _last_err; }
 protected:
@@ -26,15 +19,37 @@ private:
 };
 
 
+struct Message {
+    const char * content;
+    size_t len;
+    MessageID id;
+};
+
+
 class Queue {
 public:
-    virtual QueueProducer * create_producer() = 0;
-    virtual QueueConsumer * create_consumer() = 0;
+    virtual MsgProducer * create_producer() = 0;
+    virtual MsgConsumer * create_consumer() = 0;
     virtual QueueReader * create_reader() = 0;
 };
 
 
-class QueueReader: public _BaseObject {
+// XXX !!! need to think about how external transactions
+// to avoid duplicate produce
+class MsgProducer: public _QActor {
+public:
+    virtual bool produce(Message * msg) = 0;
+};
+
+
+// XXX !!! conflict with the reader
+class MsgConsumer: public _QActor {
+public:
+    virtual bool consume(Message * msg) = 0;
+};
+
+
+class QueueReader: public _QActor {
 public:
     // Seek to some position in the message queue.
     // Return false if seek to an unvalid position.
