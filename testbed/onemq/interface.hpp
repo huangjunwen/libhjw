0c24df7af44f0a94f6f0f28db6460ce35d86b09e
kassarar <kassarar@gmail.com>
2010-04-30 02:30:44 +0000

    Update interfaces

--- testbed/onemq/interface.hpp
+++ testbed/onemq/interface.hpp
@@ -6,19 +6,15 @@
 
 namespace omq {
 
-#define NULL_SEQ (0)
-
-
-class i_msg_t;
-class i_msg_provider_t;
-class i_msg_consumer_t;
 
 /*******************************************************
  *
  * Messages are tagged. (ordered)
  *
  *******************************************************/
-typedef struct msg_id_t {
+#define NULL_SEQ (0)
+
+typedef struct {
     msg_id_t(): uuid(NULL), seq(NULL_SEQ) {}
 
     // UUID of the msg source. (queue)
@@ -30,56 +26,78 @@ typedef struct msg_id_t {
     uint64_t seq;                                       
 } msg_id_t;
 
-class i_msg_t {
-public:
+typedef struct {
+    const unsigned char * content;
+    size_t len;
     msg_id_t id;
-};
+} msg_t;
 
 /*******************************************************
  *
  * Msg provider is responsable to provide (queued) msg. 
+ * Msg consumer is responsable to handle msg. 
  *
  *******************************************************/
+class i_msg_provider_t;
+class i_msg_consumer_t;
+
 class i_msg_provider_t {
 public:
     // A provider can bind ONLY ONE consumer. 
-    // The consumer's `msg_provided` should be called when some msg is available.
+    // Unbind the consumer if the pointer passed in is NULL.
     virtual bool bind_consumer(i_msg_consumer_t *) = 0;
     virtual i_msg_consumer_t * bound_consumer() = 0;
 
     // These can be called by the consumer.
     // Get the msg.
-    virtual i_msg_t * get_msg(const msg_id_t *) = 0;
+    virtual const msg_t * get_head_msg() = 0;
     // Called by the consumer when a msg is handled.
-    virtual bool msg_consumed(const msg_id_t *) = 0;
+    virtual bool msg_consumed(const msg_t *) = 0;
 };
 
-class i_queue_t: i_msg_provider_t {
+class i_msg_consumer_t {
 public:
-    virtual bool enqueue(const i_msg_t *) = 0;
+    // `msg_provided` should be called by the provider when some msg is available.
+    virtual void msg_provided(i_msg_provider_t *) = 0;
 };
 
 /*******************************************************
  *
- * Msg consumer is responsable to handle msg. 
+ * Component interfaces.
  *
  *******************************************************/
-class i_msg_consumer_t {
+class i_queue_t: public i_msg_provider_t {
 public:
-    virtual void msg_provided(i_msg_provider_t *, const msg_id_t *) = 0;
+    // Queue is universal unique.
+    virtual const uuid_t * get_uuid() = 0;
+
+    // Enqueue a message.
+    virtual bool enqueue(const msg_t *) = 0;
+
+    // When pause is called `get_head_msg` should return NULL 
+    // after the last outstanding msg has been consumed even the queue is not empty.
+    virtual void pause() = 0;
+    virtual void resume() = 0;
 };
 
+class i_filter_t: public i_msg_consumer_t, public i_msg_provider_t {
+public:
+    typedef bool (*filter_fn)(const msg_t *);
+    virtual void set_filter_fn(filter_fn) = 0;
+};
 
-#if 0
 class i_dispatcher_t: public i_consumer_t {
 public:
+    // Workers can be added to a dispatcher.
+    // Dispatcher dispatches msg to spare worker by some strategy.
     virtual bool add_worker(i_consumer_t *) = 0;
 };
-#endif
 
-
-
-
+// Publisher is also a consumer.
+class i_publisher_t: public i_consumer_t {
+public:
+    virtual bool add_subscriber(i_consumer_t *) = 0;
+};
 
 }
 
