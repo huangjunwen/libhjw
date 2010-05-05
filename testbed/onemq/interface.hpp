b28943e69e91baa4519dc03d6d144f22e08daa69
kassarar <kassarar@localhost>
2010-05-05 13:31:53 +0000

    Simplify interfaces

--- testbed/onemq/interface.hpp
+++ testbed/onemq/interface.hpp
@@ -26,47 +26,26 @@ typedef struct {
     uint64_t seq;                                       
 } msg_id_t;
 
-typedef struct {
+// NULL terminated string
+#define NTS (0)
+
+typedef struct msg_t {
+    msg_id_t id;
     const unsigned char * content;
     size_t len;
-    msg_id_t id;
 } msg_t;
 
 /*******************************************************
  *
- * Msg provider is responsable to provide (queued) msg. 
- * Msg consumer is responsable to handle msg. 
- *
- *******************************************************/
-class i_msg_provider_t;
-class i_msg_consumer_t;
-
-class i_msg_provider_t {
-public:
-    // A provider can bind ONLY ONE consumer. 
-    // Unbind the consumer if the pointer passed in is NULL.
-    virtual bool bind_consumer(i_msg_consumer_t *) = 0;
-    virtual i_msg_consumer_t * bound_consumer() = 0;
-
-    // These can be called by the consumer.
-    // Get the msg.
-    virtual const msg_t * get_head_msg() = 0;
-    // Called by the consumer when a msg is handled.
-    virtual bool msg_consumed(const msg_t *) = 0;
-};
-
-class i_msg_consumer_t {
-public:
-    // `msg_provided` should be called by the provider when some msg is available.
-    virtual void msg_provided(i_msg_provider_t *) = 0;
-};
-
-/*******************************************************
- *
  * Component interfaces.
+ * Queue is responsable to provide msg.
+ * Consumer is responsable to handle msg. 
  *
  *******************************************************/
-class i_queue_t: public i_msg_provider_t {
+class i_queue_t;
+class i_consumer_t;
+
+class i_queue_t {
 public:
     // Queue is universal unique.
     virtual const uuid_t * get_uuid() = 0;
@@ -74,23 +53,28 @@ public:
     // Enqueue a message.
     virtual bool enqueue(const msg_t *) = 0;
 
-    // When pause is called `get_head_msg` should return NULL 
-    // after the last outstanding msg has been consumed even the queue is not empty.
+    // A queue can be pause and resume.
     virtual void pause() = 0;
     virtual void resume() = 0;
+    // Return the number of messages that have given to the consumer but 
+    // not consumed.
+    virtual uint32_t outstand_msgs() = 0;
+
+    // A provider can bind ONLY ONE consumer. 
+    virtual bool bind_consumer(i_consumer_t *) = 0;
+    virtual i_consumer_t * bound_consumer() = 0;
+
+    // These can be called by the consumer.
+    // Get the msg.
+    virtual const msg_t * get_msg(const msg_id_t *) = 0;
+    // Called by the consumer when a msg is handled.
+    virtual bool msg_consumed(const msg_id_t *) = 0;
 };
 
-class i_filter_t: public i_msg_consumer_t, public i_msg_provider_t {
+class i_consumer_t {
 public:
-    typedef bool (*filter_fn)(const msg_t *);
-    virtual void set_filter_fn(filter_fn) = 0;
-};
-
-class i_dispatcher_t: public i_consumer_t {
-public:
-    // Workers can be added to a dispatcher.
-    // Dispatcher dispatches msg to spare worker by some strategy.
-    virtual bool add_worker(i_consumer_t *) = 0;
+    // `msg_provided` should be called by the provider when some msg is available.
+    virtual void msg_provided(i_queue_t *, const msg_id_t *) = 0;
 };
 
 // Publisher is also a consumer.
