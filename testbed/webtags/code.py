0bc76b12817326ba2393cd4ca7fe5203b458dec9
kassarar <kassarar@localhost>
2010-05-13 10:10:34 +0000

    Make handle cache Add hightlight effect when click on plain word

--- testbed/webtags/code.py
+++ testbed/webtags/code.py
@@ -1,38 +1,62 @@
 import web
 
 urls = (
-   r'/([\w\-\.]*)$', 'show_code',
-   r'/static/([\w\-\.]+)$', 'static'
+   r'/([\w\-]+(?:\.\w+)?)$', 'ShowCode',
+   r'/static/([\w\-]+(?:\.\w+)?)$', 'StaticFile'
 )
 
 app = web.application(urls, globals())
 
-class show_code:
-    def GET(self, filename):
-        if not filename:
-            raise web.notfound("Plz specified the source file name")
-        globals = {'str': str}
-        render = web.template.render('template/', globals=globals)
+# for template render
+common_globals = {
+    'str': str,
+}
+
+render = web.template.render('template/', globals=common_globals)
+
+class CacheFileMeta(type):
+    
+    def __new__(mcls, name, base, attr):
+        attr['_cache'] = {}
+
+        def enable_cache(f):
+            def ret(self, fname):
+                if fname not in self._cache:
+                    self._cache[fname] = f(self, fname)
+                    print 'haha'
+                return self._cache[fname]
+            return ret
+        
+        if 'GET' in attr:
+            attr['GET'] = enable_cache(attr['GET'])
+        if 'POST' in attr:
+            attr['POST'] = enable_cache(attr['POST'])
+            
+        return type.__new__(mcls, name, base, attr)
+
+
+class ShowCode(object):
+
+    __metaclass__ = CacheFileMeta
+
+    def GET(self, fname):
         try:
-            return render.show_code(filename, open(filename).readlines())
+            flines = open(fname).readlines()
         except IOError:
             raise web.notfound("Source file not found")
 
-class static:
-    cache = {}
-    def GET(self, filename):
-        try:
-            return self.cache[filename]
-        except KeyError:
-            pass
+        return render.show_code(fname, flines)
 
+
+class StaticFile(object):
+
+    __metaclass__ = CacheFileMeta
+
+    def GET(self, fname):
         try:
-            content = open('static/' + filename).read()
+            return open('static/' + fname).read()
         except IOError:
             raise web.notfound("File not found")
-        
-        self.cache[filename] = content
-        return content
         
 
 if __name__ == "__main__": 
