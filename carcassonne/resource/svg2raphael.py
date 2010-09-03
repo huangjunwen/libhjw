3547fee4e9d6bc31ea6deb74a02e1cb035386219
kassarar <kassarar@localhost>
2010-09-03 06:59:49 +0000

    Update svg2raphael.py

--- carcassonne/resource/svg2raphael.py
+++ carcassonne/resource/svg2raphael.py
@@ -2,29 +2,31 @@ import sys, re
 import warnings
 import xml.etree.ElementTree as ET
 
-class Svg2Raphael(object):
+class Svg2RaphaelHandler(object):
     """
     Extend this class directly if more svg xml tag needs to be support
     """
 
-    def __init__(self, funcName):
+    def __init__(self, funcName=""):
         self.funcName = funcName
-        self.contVarName = "canvas"
+        self.transformPat = re.compile(r"(\w+)\([^\)]+\)")
+        self.raphaelTransforms = ("rotate", "translate", "scale")
 
-    transformPat = re.compile(r"(\w+)\([^\)]+\)")
-    raphaelTransform = ("rotate", "translate", "scale")
     def _getElemAttr(self, elem):
         attr = elem.attrib
+
+        # http://raphaeljs.com/reference.html#attr
         style = attr.get("style", "")
         if style:
             style = '.attr("' + style.replace(':', '","').replace(';','").attr("') + '")'
 
         # http://www.w3.org/TR/SVG/coords.html#TransformAttribute
+        # http://raphaeljs.com/reference.html#rotate ...
         transform = attr.get("transform", "")
         if transform:
             t = []
             for m in self.transformPat.finditer(transform):
-                if m.group(1) not in self.raphaelTransform:
+                if m.group(1) not in self.raphaelTransforms:
                     warnings.warn("raphael not support transform %s" % m.group(1))
                     continue
                 t.append(m.group(0))
@@ -32,32 +34,22 @@ class Svg2Raphael(object):
         return style + transform
 
     def svg(self, elem, childRes):
-        sep = ";"
-        tmpl = sep.join(["function %s(contID){var %s=Raphael(contID,%s,%s)", 
-            "%s", 
-            "return %s", 
-            "}"]
-        )
-        attr = elem.attrib
-        return tmpl % (self.funcName, self.contVarName, attr['width'], 
-            attr['height'], 
-            sep.join(childRes),
-            self.contVarName,
+        # convert <svg> to <g> so that one canvas can hold multiple objects.
+        return "function %s(canvas){return canvas.set().push(%s);}" % (
+            self.funcName, 
+            ','.join(childRes),
         )
     
     def g(self, elem, childRes):
         if not childRes:
             return
-        sep = ","
-        return "%s.set().push(%s)%s" % (self.contVarName, 
-            sep.join(childRes),
+        return "canvas.set().push(%s)%s" % (','.join(childRes),
             self._getElemAttr(elem),
         )
 
     def rect(self, elem, _):
         attr = elem.attrib
-        return "%s.rect(%s,%s,%s,%s)%s" % (self.contVarName,
-            attr['x'],
+        return "canvas.rect(%s,%s,%s,%s)%s" % (attr['x'],
             attr['y'],
             attr['width'],
             attr['height'],
@@ -66,9 +58,7 @@ class Svg2Raphael(object):
     
     def path(self, elem, _):
         attr = elem.attrib
-        return "%s.path('%s')%s" % (self.contVarName,
-            attr['d'],
-            self._getElemAttr(elem),
+        return "canvas.path('%s')%s" % (attr['d'], self._getElemAttr(elem),
         )
     
     # ... more ...
@@ -90,16 +80,23 @@ def traverse(elem, handler):
     childRes = [traverse(child, handler) for child in elem.getchildren()]
     return h(elem, [r for r in childRes if r is not None])
 
+def svg2raphael(f, *args, **kw):
+    """
+    @param f: a svg file object
+    @param args, kw: see Svg2RaphaelHandler's __init__
+    @return: javascript function code to produce the svg file
+    """
+    svg = ET.parse(f).getroot()
+    assert svg.tag == svgPrefix + "svg"
+    return traverse(svg, Svg2RaphaelHandler(*args, **kw))
+
 def main():
     if len(sys.argv) != 2:
         print "Usage:"
         print "\tpython %s <svg file name>" % __file__
         return
     filename = sys.argv[1]
-    svg = ET.parse(open(filename)).getroot()
-    assert svg.tag == svgPrefix + "svg"
-    return traverse(svg, Svg2Raphael("svg" + filename.split('.')[0]))
-
+    return svg2raphael(open(filename), "svg" + filename.split('.')[0])
 
 if __name__ == '__main__':
     print main()
