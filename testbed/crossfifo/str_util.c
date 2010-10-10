ed4f990c294b1cf699d55e09be54341a7b285a7e
kassarar <kassarar@localhost>
2010-10-10 04:30:40 +0000

    A string pool is unnecessary

--- testbed/crossfifo/str_util.c
+++ testbed/crossfifo/str_util.c
@@ -1,51 +1,23 @@
 #include <string.h>
-#include <assert.h>
 #include "str_util.h"
-
-void ref_str_pool_init(ref_str_pool_t * pool) {
-    ref_str_t * head = &pool->head;
-    head->ref_count = 1;                // owned by the pool itself
-    head->length = 0;
-    head->next = head->prev = head;
-    head->str[0] = '\0';
-}
-
-void ref_str_pool_destory(ref_str_pool_t * pool) {
-    ref_str_t * head = &pool->head;
-    // there should be only one left
-    assert(head->next == head && head->ref_count == 1);
-}
 
 static const size_t REF_STR_BASE_SIZE = (size_t)(((ref_str_t *)0)->str);
 
-const char * ref_str_add(ref_str_pool_t * pool, const char * str) {
-    if (!str[0]) {
-        ++pool->head.ref_count;
-        return &(pool->head.str);
-    }
-    size_t len = strlen(str);
-    ref_str_t * rs = (ref_str_t *)malloc(REF_STR_BASE_SIZE + len + 1);
-    rs->ref_count = 1;                  // owned by the caller
-    rs->length = len;
-    rs->next = pool->head.next;         // link
-    rs->prev = &pool->head;
-    pool->head.next->prev = rs;
-    pool->head.next = rs;
-    strcpy(rs->str, str);
-    return rs->str;
+const char * make_ref_str(const char * s) {
+    size_t len = strlen(s);
+    ref_str_t * ref_str = (ref_str_t *)malloc(REF_STR_BASE_SIZE + len + 1);
+    ref_str->ref_count = 1;                  // owned by the caller
+    ref_str->length = len;
+    strcpy(ref_str->str, s);
+    return ref_str->str;
 }
 
-const char * ref_str_dup(const char * ref_str) {
-    ++((ref_str_t *)(ref_str - REF_STR_BASE_SIZE)->ref_count);
-    return ref_str;
+void ref_str_incref(const char * rs) {
+    ++(((ref_str_t *)(rs - REF_STR_BASE_SIZE))->ref_count);
 }
 
-void ref_str_free(const char * ref_str) {
-    ref_str_t * rs = (ref_str_t *)(ref_str - REF_STR_BASE_SIZE);
-    if (--rs->ref_count <= 0) {
-        assert(ref_str[0]);             // must not be the head
-        rs->next->prev = rs->prev;      // unlink
-        rs->prev->next = rs->next;
-        free(rs);
-    }
+void ref_str_decref(const char * rs) {
+    ref_str_t * ref_str = (ref_str_t *)(rs - REF_STR_BASE_SIZE);
+    if (--ref_str->ref_count <= 0)
+        free(ref_str);
 }
--- testbed/crossfifo/str_util.h
+++ testbed/crossfifo/str_util.h
@@ -4,22 +4,13 @@
 typedef struct ref_str_t {
     unsigned int ref_count;             // ref count of this NULL-term string
     unsigned int length;                // length of the string (not include '\0')
-    struct ref_str_t * next;
-    struct ref_str_t * prev;
     char str[1];                        // dyn-length struct
 } ref_str_t;
 
 
-typedef struct ref_str_pool_t {
-    struct ref_str_t head;              // head is '\0'
-} ref_str_pool_t;
-
-void ref_str_pool_init(ref_str_pool_t * pool);
-void ref_str_pool_destory(ref_str_pool_t * pool);
-
-// one should call ref_str_free for each ref_str_add and ref_str_dup
-const char * ref_str_add(ref_str_pool_t * pool, const char * str);
-const char * ref_str_dup(const char * ref_str);
-void ref_str_free(const char * ref_str);
+// one should call ref_str_decref for each make_ref_str and ref_str_incref
+const char * make_ref_str(const char * s);
+void ref_str_incref(const char * rs);
+void ref_str_decref(const char * rs);
 
 #endif // _CFF_STR_UTIL_H_
