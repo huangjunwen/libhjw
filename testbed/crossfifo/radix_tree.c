bb8dbfd9ac4143b2b8d0baf0751dcc366a3c1524
kassarar <kassarar@localhost>
2010-10-13 10:18:55 +0000

    Half implement radix_tree

--- testbed/crossfifo/radix_tree.c
+++ testbed/crossfifo/radix_tree.c
@@ -1,7 +1,39 @@
 #include <assert.h>
 #include <string.h>
-#include "ref_obj.h"
+#include "str_util.h"
 #include "radix_tree.h"
+
+static const unsigned char _bit_mask[8] = {128,64,32,16,8,4,2,1};
+
+static unsigned char _get_bit(const char * s, unsigned int len, int bit_idx) {
+    return (bit_idx < 0 || bit_idx >= len << 3) ? 0 :
+        (s[bit_idx >> 3] & _bit_mask[bit_idx & 7]);
+}
+
+// call this only when the two strings are different, it doesn't check
+// end bound
+static unsigned int _diff_bit_idx(const char * s1, const char * s2) {
+    const char * p1, * p2;
+    char c;
+    unsigned int i, j;
+
+    p1 = s1;
+    p2 = s2;
+    i = 0;
+    while (*p1 == *p2) {
+        ++i;
+        ++p1;
+        ++p2;
+    }
+    i <<= 3;                    // *= 8
+
+    c = (*p1) ^ (*p2);
+    while ((c & 128) == 0) {
+        c <<= 1;
+        ++i;
+    }
+    return i;
+}
 
 kv_list_t * kv_list_create(unsigned int elem_size) {
     if (elem_size < sizeof(kv_t))
@@ -20,9 +52,8 @@ kv_t * kv_list_insert(kv_list_t * list, const char * key, void * val) {
     kv_t * ret = (kv_t *)malloc(list->elem_size);
     if (!ret)
         return NULL;
-    ref_obj_incref(key);
+    ref_str_incref(key);
     ret->key = key;
-    ref_obj_incref(val);
     ret->val = val;
     ret->next = list->head.next;
     ret->prev = &list->head;
@@ -33,9 +64,7 @@ kv_t * kv_list_insert(kv_list_t * list, const char * key, void * val) {
 }
 
 void kv_list_remove(kv_list_t * list, kv_t * kv) {
-    assert(kv != &list->head);
-    ref_obj_decref(kv->key);
-    ref_obj_decref(kv->val);
+    ref_str_decref(kv->key);
     kv->prev->next = kv->next;
     kv->next->prev = kv->prev;
     free(kv);
@@ -47,3 +76,52 @@ void kv_list_destory(kv_list_t * list) {
         kv_list_remove(list, list->head.next);
 } 
 
+radix_tree_t * radix_tree_create() {
+    radix_tree_t * ret = (radix_tree_t *)malloc(sizeof(radix_tree_t));
+    if (!ret)
+        return NULL;
+    ret->node_list = kv_list_create(sizeof(rt_node_t));
+    if (!ret->node_list) {
+        free(ret);
+        return NULL;
+    }
+    ret->root = NULL;
+    return ret;
+}
+
+void radix_tree_destory(radix_tree_t * tree) {
+}
+
+rt_node_t * radix_tree_find(radix_tree_t * tree, const char * key) {
+    rt_node_t * p;  // parent
+    rt_node_t * c;  // child
+    
+    // ...
+
+    unsigned int klen = strlen(key);
+    while (c->bit_idx > p->bit_idx) {
+        p = c;
+        c = _get_bit(key, klen, c->bit_idx) ? c->right : c->left;
+    }
+    if (strncmp(key, c->kv.key))
+        return NULL;
+    return c;
+}
+
+rt_node_t * radix_tree_insert(struct radix_tree_t * tree, 
+        const char * key, 
+        void * val) {
+    rt_node_t * p;  // parent
+    rt_node_t * c;  // child
+    
+    // ...
+
+    unsigned int klen = strlen(key);
+    while (c->bit_idx > p->bit_idx) {
+        p = c;
+        c = _get_bit(key, klen, c->bit_idx) ? c->right : c->left;
+    }
+    if (strcmp(key, c->kv.key))
+        return NULL;
+    return c;
+}
--- testbed/crossfifo/radix_tree.h
+++ testbed/crossfifo/radix_tree.h
@@ -1,7 +1,7 @@
 #ifndef _CFF_RADIX_TREE_H_
 #define _CFF_RADIX_TREE_H_
 
-/*  kv_list_t stands for a group of key-value pairs */
+/* kv_list_t stands for a group of key-value pairs */
 
 typedef struct kv_t {
     const char * key;           // NULL-term referenced string
@@ -16,8 +16,7 @@ typedef struct kv_list_t {
     unsigned int elem_size;
 } kv_list_t;
 
-/*  radix_tree_t use radix tree to boost the operations
- *  on the group of key-value pairs */
+/* radix tree */
 
 typedef struct radix_tree_node_t {
     kv_t kv;
@@ -27,12 +26,13 @@ typedef struct radix_tree_node_t {
 } rt_node_t;
 
 typedef struct radix_tree_t {
-    kv_list_t node_list;
+    kv_list_t * node_list;
     struct radix_tree_node_t * root;
 } radix_tree_t;
 
-/* operations on kv */
+/* operations */
 
+// kv ops
 kv_list_t * kv_list_create(unsigned int elem_size);
 
 void kv_list_destory(kv_list_t * list);
@@ -41,15 +41,18 @@ kv_t * kv_list_insert(kv_list_t * list, const char * key, void * val);
 
 void kv_list_remove(kv_list_t * list, kv_t * kv);
 
-/* operations on radix tree */
+// radix tree ops
+radix_tree_t * radix_tree_create();
+
+void radix_tree_destory(radix_tree_t * tree);
+
+rt_node_t * radix_tree_find(radix_tree_t * tree, const char * key);
+
+rt_node_t * radix_tree_insert(struct radix_tree_t * tree, 
+        const char * key, 
+        void * val);
 
 #if 0
-void radix_tree_init(struct radix_tree_t * tree);
-
-void radix_tree_destory(struct radix_tree_t * tree);
-
-rt_node_t * radix_tree_find(struct radix_tree_t * tree, const char * key);
-
 rt_node_t * radix_tree_insert(struct radix_tree_t * tree, 
         const char * key, 
         void * val);
